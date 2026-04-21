9a014427b14c245f6cdaae3cb56c683cea7f0d15
abusstore12-oss <abusstore12@gmail.com>
2026-04-21 10:05:01 +0200

    Create Dashboard component

---
+++ src/components/Dashboard.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Dashboard = () => {
+    return (
+        <div style={{ background: 'linear-gradient(to right, #ff7e5f, #feb47b)', padding: '20px' }}>
+            <h1>Dashboard</h1>
+            {/* Additional Dashboard functionalities will be implemented here */}
+        </div>
+    );
+};
+
+export default Dashboard;
